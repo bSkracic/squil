146d3b9997b04d349c6fea58695593e23fd21078
Borna Skracic <borna.skracic7@gmail.com>
2021-07-14 16:24:39 +0200

    added new initializer

--- src/main/java/hr/bskracic/squil/SquilApplication.java
+++ src/main/java/hr/bskracic/squil/SquilApplication.java
@@ -2,12 +2,19 @@ package hr.bskracic.squil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SquilApplication {
+public class SquilApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SquilApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SquilApplication.class);
 	}
 
 }
